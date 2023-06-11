424abbc802d59ffcd5d550cdd6f50696b2f69eb1
stefmitkovski <mitkovski.stefan.sm@gmail.com>
2023-06-11 16:06:06 +0200

    Initial commit(only the first version of the design)

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    CityAdapter cityAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> values = Arrays.asList("Paris", "London", "Lisbon","Kiev","Moscow","Prague","Rome","Copenhagen","Bucharest");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list_cities);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        cityAdapter = new CityAdapter(values, R.layout.city_row, this);
+
+        mRecyclerView.setAdapter(cityAdapter);
+    }
+
+}
